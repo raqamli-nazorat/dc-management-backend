fccd8d2cc37de78807458c9eaf9c4291a7028203
Alijonov <alijonov.me@gmail.com>
2026-05-05 10:49:18 +0500

    Loyiha muzlatilganida va muzlatilgan loyiha qayta faollashtirilganida chiqadigan muammolar bartaraf etildi

--- config/settings.py
+++ config/settings.py
@@ -152,7 +152,7 @@ CELERY_TASK_REJECT_ON_WORKER_LOST = True
 CELERY_BEAT_SCHEDULE = {
     'check-deadlines-every-10-min': {
         'task': 'apps.projects.tasks.update_overdue_status_and_notify',
-        'schedule': crontab(minute='*/10'),
+        'schedule': crontab(minute='*'),
     },
 
     'morning-task-reminders': {
